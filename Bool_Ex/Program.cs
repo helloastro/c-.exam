8e7512121977e0a8e4a4fc2b7807c8ebdce9805a
helloastro <45805203+helloastro@users.noreply.github.com>
2021-04-15 12:40:08 +0900

    Create Program.cs

---
+++ Bool_Ex/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Bool_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+
+            Console.WriteLine($"{a}, {b}");
+        }
+    }
+}
