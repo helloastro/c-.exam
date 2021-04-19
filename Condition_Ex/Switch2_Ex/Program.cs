04f0ca1d575254e57b1b86fe348549f8548419f7
helloastro <45805203+helloastro@users.noreply.github.com>
2021-04-19 16:54:47 +0900

    Update Program.cs

--- Condition_Ex/Switch2_Ex/Program.cs
+++ Condition_Ex/Switch2_Ex/Program.cs
@@ -6,25 +6,24 @@ namespace Switch2_Ex
     {
         static void Main(string[] args)
         {
-            Console.Write("점수를 입력해 주세요: ");
+            Console.Write("점수를 입력해 주세요.: ");
             int score = Convert.ToInt32(Console.ReadLine());
 
-            // Switch 식
-            string grade = ((score/10)*10) switch {
+            string grade = ((score / 10) * 10) switch
+            {
                 90 => "A",
                 80 => "B",
                 70 => "C",
                 60 => "D",
                 _ => "F"
             };
-
             Console.WriteLine($"학점: {grade}");
 
-            Console.Write("재수강 인가요?(y/n): ");
+            Console.WriteLine("재수강 인가요?(y/n): ");
             string repeated = Console.ReadLine();
-            bool repeated_yn = repeated == "y" ? true : false;
+            bool repeated_yn = (repeated == "y") ? true:false;
 
-            string grade1 = (int)(Math.Truncate(score / 10.0) * 10) switch
+            string grade1 = ((score / 10) * 10) switch   // 결과값이 return.
             {
                 90 when repeated_yn == true => "B+",
                 90 => "A",
@@ -33,8 +32,16 @@ namespace Switch2_Ex
                 60 => "D",
                 _ => "F"
             };
-
             Console.WriteLine($"학점: {grade1}");
+
+            grade = ((score / 10) * 10) switch
+            {
+                var x when x == 90 => "A",
+                var x when x < 90 && x >=70 => "B",
+                60 => "C",
+                _ => "F"
+            };
+            Console.WriteLine($"학점: {grade}");
         }
     }
 }
