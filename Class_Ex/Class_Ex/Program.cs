92e9af6f87e7dc205fb435fe3ccceda10dd12056
helloastro <45805203+helloastro@users.noreply.github.com>
2021-04-20 16:50:20 +0900

    Update Program.cs

--- Class_Ex/Class_Ex/Program.cs
+++ Class_Ex/Class_Ex/Program.cs
@@ -7,12 +7,31 @@ namespace Class_Ex
         public string Name;
         public string Color;
 
-        public void Meow()
+        // 기본 생성자
+        public Cat()
         {
-            Console.WriteLine($"{Name}({Color}) : 야옹");
+            Name = "";
+            Color = "";
+        }
+
+        // 생성자 오버로딩
+        public Cat(string _name, string _color)
+        {
+            Name = _name;
+            Color = _color;
+        }
+
+        public void Meow() {
+            Console.WriteLine("야옹");
+        }
+
+        // 소멸자
+        ~Cat()
+        {
+            Console.WriteLine($"{Name} : 잘가");
         }
     }
-
+    
     class Program
     {
         static void Main(string[] args)
@@ -24,8 +43,12 @@ namespace Class_Ex
 
             Cat nero = new Cat();
             nero.Name = "네로";
-            nero.Color = "검은색";
+            nero.Color = "검정색";
             nero.Meow();
+
+            Cat kitty2 = new Cat(_name:"키티2", "하얀색");
+            kitty2.Meow();
+            Console.WriteLine($"{kitty2.Name}, {kitty2.Color}");
         }
     }
 }
